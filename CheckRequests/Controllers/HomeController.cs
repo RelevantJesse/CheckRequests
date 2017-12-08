b241a3528d5510dda33517a1b61485ac88626088
Jesse Prescott <jesse.prescott@havasedge.com>
2017-12-08 12:04:32 -0800

    Added database entities

--- CheckRequests/Controllers/HomeController.cs
+++ CheckRequests/Controllers/HomeController.cs
@@ -3,26 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CheckRequests.Models;
 
 namespace CheckRequests.Controllers
 {
     public class HomeController : Controller
     {
+        readonly VantEDGEPointEntities _context = new VantEDGEPointEntities();
+
         public ActionResult Index()
         {
-            return View();
+            List<check_request> activeCheckRequests = new List<check_request>();
+            
+
+            activeCheckRequests = _context.check_request.Where(x => x.business_action != 1 &&
+                                                                       x.business_action != 2).ToList();
+
+            return View(activeCheckRequests);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var details = _context.check_request_detail.FirstOrDefault(x => x.check_request == id);
+            return View(details);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
